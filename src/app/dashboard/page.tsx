41e6289e3ff36ea34566f38e155e963ff72c1cac
Maimeetang <sirawit.36298@gmail.com>
2025-09-29 19:44:50 +0700

    test get profile

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import Navbar from "@/component/Navbar";
+import { User } from "@/types/User";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function dashboard() {
-  return <div>dashboard</div>;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    axios.get<User>("api/auth/profile").then((res) => {
+      setUser(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Navbar userName="Sirawit Kongkham" />
+      <div className="p-3">
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+      </div>
+    </>
+  );
 }
---
+++ src/types/User.ts
@@ -0,0 +1,17 @@
+export interface User {
+  cmuitaccount: string;
+  cmuitaccount_name: string;
+  exp: number;
+  firstname_EN: string;
+  firstname_TH: string;
+  iat: number;
+  itaccounttype_EN: string;
+  itaccounttype_TH: string;
+  itaccounttype_id: string;
+  lastname_EN: string;
+  lastname_TH: string;
+  organization_name_EN: string;
+  organization_name_TH: string;
+  role: string;
+  student_id: string;
+}
