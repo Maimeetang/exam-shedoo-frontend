b88a331508c0368edbe3453fd90fabf2d86b643c
NokiaTh131 <nokiath131@gmail.com>
2025-10-07 02:43:46 +0700

    feat: add profile image

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -12,16 +12,21 @@ export default function Navbar({ profile }: prop) {
   return (
     <nav className="flex items-center justify-end bg-[#AD92B2] text-white px-6 py-3 h-20">
       <div className="flex items-center space-x-10 relative">
-        <img src="/profile.png" alt="profile" className="h-12 w-auto" />
-        <div className="flex flex-col items-center">
-          <p className="font-normal text-black">{`${profile.firstname_EN} ${profile.lastname_EN}`}</p>
-          {profile.role === "student" && (
-            <p className="font-normal text-black">{profile.student_id}</p>
-          )}
+        <div className="flex items-center space-x-5 relative">
+          <img
+            src={`https://api.dicebear.com/9.x/notionists/svg?seed=${profile.student_id}`}
+            alt="profile"
+            className="h-12 w-auto bg-white rounded-full"
+          />
+          <div className="flex flex-col items-center">
+            <p className="font-normal text-black">{`${profile.firstname_EN} ${profile.lastname_EN}`}</p>
+            {profile.role === "student" && (
+              <p className="font-normal text-black">{profile.student_id}</p>
+            )}
+          </div>
         </div>
-        {profile.role === "professor" ||
-          (profile.role === "admin" && <DropdownMenu />)}
-        {profile.role === "student" && (
+        {profile.role === "admin" && <DropdownMenu />}
+        {profile.role === "professor" || profile.role === "student" && (
           <Link href="/logout">
             <p className="text-black font-medium underline">Log out</p>
           </Link>
