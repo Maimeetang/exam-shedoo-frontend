d4b2243e4cd5841b2bc096cf097790529b311356
Maimeetang <sirawit.36298@gmail.com>
2025-10-15 03:05:00 +0700

    docker compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Stage 1: Build
+FROM node:20-alpine AS builder
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+COPY . .
+COPY .env .env
+RUN npm run build
+
+# Stage 2: Production
+FROM node:20-alpine
+WORKDIR /app
+COPY --from=builder /app ./
+EXPOSE 3000
+CMD ["npm", "run", "start"]
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
+const API_HOST = process.env.NEXT_PROXY;
 
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/:path*",
+        destination: `${API_HOST}/:path*`,
       },
     ];
   },
