49bb147d5cf60b0a19a030a8f158ee28bc8ec5c4
Maimeetang <sirawit.36298@gmail.com>
2025-10-19 02:36:44 +0700

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
-const API_HOST = process.env.NEXT_PROXY;
 
 const nextConfig: NextConfig = {
-  env: {
-    CMU_ENTRAID_URL: process.env.CMU_ENTRAID_URL,
-  },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${API_HOST}/:path*`,
+        destination: `${process.env.NEXT_PROXY}/:path*`,
       },
     ];
   },
