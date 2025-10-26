01ee27a175ccab4e70f6d648aedee961db2df048
NokiaTh131 <nokiath131@gmail.com>
2025-10-26 13:30:43 +0700

    feat: add fallback for CMU_ENTRAID_URL

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   publicRuntimeConfig: {
-    CMU_ENTRAID_URL: process.env.CMU_ENTRAID_URL,
+    CMU_ENTRAID_URL: process.env.CMU_ENTRAID_URL || "",
   },
   async rewrites() {
     return [
